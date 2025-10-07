2c72caf8459dfe6fd5b127c6307bb99f55715689
waseem <waseemabbaskpr786@gmail.com>
2025-10-07 11:15:46 -0700

    add a edit button for customers to edit their details

---
+++ Tailor_App/View/EditCustomerView.swift
@@ -0,0 +1,18 @@
+//
+//  EditCustomerView.swift
+//  Tailor_App
+//
+//  Created by Waseem Abbas on 07/10/2025.
+//
+
+import SwiftUI
+
+struct EditCustomerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditCustomerView()
+}
